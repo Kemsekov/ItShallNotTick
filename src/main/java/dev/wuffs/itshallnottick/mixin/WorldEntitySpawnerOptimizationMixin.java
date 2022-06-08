28844e874062d2ca90ca8085f8dff7abac890679
Gareth Williams <gaz492@gmail.com>
2022-06-08 19:10:22 +0100

    Fix crash

--- src/main/java/dev/wuffs/itshallnottick/mixin/WorldEntitySpawnerOptimizationMixin.java
+++ src/main/java/dev/wuffs/itshallnottick/mixin/WorldEntitySpawnerOptimizationMixin.java
@@ -5,6 +5,7 @@ import dev.wuffs.itshallnottick.Config;
 import dev.wuffs.itshallnottick.Utils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.level.NaturalSpawner;
 import net.minecraft.world.level.StructureManager;
@@ -16,12 +17,11 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Optional;
-import java.util.Random;
 
 @Mixin(NaturalSpawner.class)
 public class WorldEntitySpawnerOptimizationMixin {
     @Inject(at = @At("HEAD"), method = "getRandomSpawnMobAt", cancellable = true)
-    private static void getRandomSpawnMobAt(ServerLevel level, StructureManager arg2, ChunkGenerator arg3, MobCategory arg4, Random random, BlockPos blockPos, CallbackInfoReturnable<Optional<MobSpawnSettings.SpawnerData>> cir) {
+    private static void getRandomSpawnMobAt(ServerLevel level, StructureManager arg2, ChunkGenerator arg3, MobCategory arg4, RandomSource arg5, BlockPos blockPos, CallbackInfoReturnable<Optional<MobSpawnSettings.SpawnerData>> cir) {
         if (!Utils.isInClaimedChunk(level, blockPos) && Utils.enoughPlayers(level)) {
             int maxHeight = Config.maxEntitySpawnDistanceVertical.get();
             int maxDistanceSquare = Config.maxEntitySpawnDistanceHorizontal.get();
